936e3aece94d877ab157397ae802b0c435033012
CarloMonroy <carlomonroy1997@gmail.com>
2022-08-05 17:43:52 -0500

    Add functions to handle copy to clipboard

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ encriptarButton.onclick = encriptar;
 const decriptButton = document.querySelector(".decript-button");
 decriptButton.onclick = desencriptar;
 
+const copyButton = document.querySelector(".copy-button");
+copyButton.onclick = copyToClipboard;
+
 function showResult(text) {
   document.querySelector("#p-1").style.display = "none";
   document.querySelector("#p-2").style.display = "none";
@@ -14,13 +17,23 @@ function showResult(text) {
   document.querySelector("#p-3").innerHTML = text;
 }
 
+function copyToClipboard() {
+  const cb = navigator.clipboard;
+  const paragraph = document.querySelector("#p-3");
+  cb.writeText(paragraph.innerHTML);
+}
+
 function desencriptar() {
   let value = documentTextInput.value
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "")
     .toLowerCase(); //Removing accents, code from stackoverflow https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
-  let valueArray = value.split(""); //Also making the string lowercase
-  let decriptedString = "";
+  let decriptedString = value.replace(/enter/g, "e");
+  decriptedString = decriptedString.replace(/imes/g, "i");
+  decriptedString = decriptedString.replace(/ai/g, "a");
+  decriptedString = decriptedString.replace(/ober/g, "o");
+  decriptedString = decriptedString.replace(/ufat/g, "u");
+  showResult(decriptedString);
 }
 
 function encriptar() {
